0ba5c068c40c34c1acc6d9d8831be86b5c73363a
gmarler <gmarler@gmarler.com>
2016-12-30 16:31:11 -0500

    Start work on Host Service Tests

--- src/app/host.service.spec.ts
+++ src/app/host.service.spec.ts
@@ -1,6 +1,8 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, async, inject, fakeAsync, tick } from '@angular/core/testing';
+import {MockBackend} from '@angular/core/testing';
+import {Http, ConnectionBackend, BaseRequestOptions, Response, ResponseOptions} from '@angular/http';
 import { HostService } from './host.service';
 
 describe('HostService', () => {
