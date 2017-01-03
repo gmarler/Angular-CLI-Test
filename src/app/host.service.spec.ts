1d6a45839d9779e245be4bbcd064ee5590cb641e
gmarler <gmarler@gmarler.com>
2017-01-03 10:04:27 -0500

    Begin test work for HostService

--- src/app/host.service.spec.ts
+++ src/app/host.service.spec.ts
@@ -1,14 +1,25 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject, fakeAsync, tick } from '@angular/core/testing';
-import {MockBackend} from '@angular/core/testing';
-import {Http, ConnectionBackend, BaseRequestOptions, Response, ResponseOptions} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+import {Http, ConnectionBackend, BaseRequestOptions} from '@angular/http';
 import { HostService } from './host.service';
 
 describe('HostService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HostService]
+      providers: [
+        HostService,
+        MockBackend,
+        BaseRequestOptions,
+        { provide: Http,
+          useFactory: (backend: ConnectionBackend,
+                       defaultOptions: BaseRequestOptions) => {
+                         return new Http(backend, defaultOptions);
+                       },
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ]
     });
   });
 
--- src/app/host.service.ts
+++ src/app/host.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class HostService {
+  static BASE_URL: string = 'http://nydevsol10:5000/hosts';
 
-  constructor() { }
+  constructor(private http: Http) {
+  }
+
+  query(URL: string, params?: Array<string>): Observable<any[]> {
+    let queryURL: string = `${HostService.BASE_URL}${URL}`;
+
+    // TODO: Add params handling
+    // if (params) {
+    //   queryURL = `${queryURL}?${params.join('&')}`;
+    // }
+
+    return this.http.request(queryURL).map((res: any) => res.json());
+  }
 
 }
