fdcb14be87f1905f30f24d6f8cca35c65ec81999
gmarler <gmarler@gmarler.com>
2017-01-11 17:25:40 -0500

    Work on Host Service

--- src/app/host.service.ts
+++ src/app/host.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
-import {Http} from "@angular/http";
-import {Observable} from "rxjs";
+import {Http} from '@angular/http';
+import {Observable, BehaviorSubject, Subject} from 'rxjs';
 import 'rxjs/add/operator/map';
-import {environment} from "../environments/environment";
+import {environment} from '../environments/environment';
+import {Host} from './host';
+
+
+// TODO: This service should just be to provide the list of PA Server hosts
 
 @Injectable()
 export class HostService {
   private envName;
+  private currentPAServer;
+  private currentHost: Subject<Host> = new BehaviorSubject<Host>(null);
 
   // Based on whether we are in dev or prod environment, make the SVC_HOST and SVC_PORT configurable
   private SVC_HOST: string = 'nydevsol10';
@@ -15,20 +21,23 @@ export class HostService {
 
   constructor(private http: Http) {
     if (environment.production) {
-      this.envName = "production";
+      this.envName = 'production';
       this.SVC_PORT = '80';
       this.BASE_URL = `http://${this.SVC_HOST}:${this.SVC_PORT}`;
     } else {
-      this.envName = "dev";
+      this.envName = 'dev';
       this.SVC_PORT = '5000';
       this.BASE_URL = `http://${this.SVC_HOST}:${this.SVC_PORT}`;
     }
     console.log(`ENVIRONMENT: ${this.envName}`);
     console.log(`BASE_URL: ${this.BASE_URL}`);
+
+    // TODO: Use this to create BASE_URL
+    this.currentPAServer = this.SVC_HOST;
   }
 
   query(URL: string, params?: Array<string>): Observable<any[]> {
-    let queryURL: string = `${this.BASE_URL}${URL}`;
+    let queryURL = `${this.BASE_URL}${URL}`;
 
     // TODO: Add params handling
     // if (params) {
