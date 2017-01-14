a04784f929b98bd4a57235772bcc6bc9a2c59ece
gmarler <gmarler@gmarler.com>
2017-01-14 12:49:45 -0500

    syntax corrections

--- src/app/host.service.ts
+++ src/app/host.service.ts
@@ -68,9 +68,9 @@ export class HostService {
     // TODO: Use another logging infrastructure
     let errMsg:  string;
     if (error instanceof Response) {
-      const body = error.json || '';
-      const err = body.error | JSON.stringify(body);
-      errMsg = `${error.status} - $error.statusText || ''} ${err}`;
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
