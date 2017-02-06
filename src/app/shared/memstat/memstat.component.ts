82754e593e5db6a97d7567b742bc878f8531faff
gmarler <gmarler@gmarler.com>
2017-02-06 15:08:18 -0500

    Set up for input and data changes

--- src/app/shared/memstat/memstat.component.ts
+++ src/app/shared/memstat/memstat.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnChanges, Input} from '@angular/core';
 
 @Component({
   selector: 'app-memstat',
   templateUrl: './memstat.component.html',
   styleUrls: ['./memstat.component.css']
 })
-export class MemstatComponent implements OnInit {
+export class MemstatComponent implements OnInit, OnChanges {
+
+  @Input() private data: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    if (this.chart) {
+      this.updateChart();
+    }
   }
 
 }
