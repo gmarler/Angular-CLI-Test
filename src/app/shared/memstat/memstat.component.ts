3a1dceee31501267af3109b4db978527ef166b2f
gmarler <gmarler@gmarler.com>
2017-02-06 15:15:05 -0500

    private members.

--- src/app/shared/memstat/memstat.component.ts
+++ src/app/shared/memstat/memstat.component.ts
@@ -1,13 +1,28 @@
-import {Component, OnInit, OnChanges, Input} from '@angular/core';
+import {Component, OnInit, OnChanges, Input, ViewChild, ElementRef} from '@angular/core';
+import * as d3 from 'd3';
+
+// From Example at:
+// https://github.com/keathmilligan/angular2-d3-v4
 
 @Component({
   selector: 'app-memstat',
   templateUrl: './memstat.component.html',
   styleUrls: ['./memstat.component.css']
+  // encapsulation: ViewEncapsulation.None
 })
 export class MemstatComponent implements OnInit, OnChanges {
-
+  @ViewChild('chart') private chartContainer: ElementRef;
   @Input() private data: Array<any>;
+
+  private chart: any;
+  private margin: any = { top: 0, bottom: 0, left: 0, right: 0 };
+  private width:  number;
+  private height: number;
+  private xScale:  any;
+  private yScale:  any;
+  private colors:  any;
+  private xAxis:   any;
+  private yAxis:   any;
 
   constructor() { }
 
@@ -20,4 +35,12 @@ export class MemstatComponent implements OnInit, OnChanges {
     }
   }
 
+  createChart() {
+
+  }
+
+  updateChart() {
+
+  }
 }
+
